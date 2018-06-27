7f61e717440053372cddaa15b75d2e64a3b63159
AtharvPawar <40492769+AtharvPawar@users.noreply.github.com>
2018-06-27 15:35:51 +0530

    Update Main_Arduino.ino

--- Main_Arduino/Main_Arduino.ino
+++ Main_Arduino/Main_Arduino.ino
@@ -6,9 +6,12 @@ const int stepPin2 = 2;
 const int dirPin2 = 3;
 const int stepTime = 12;
 
-char inChar = '\0';
 
 SoftwareSerial BT(5, 6); //TX, RX from Arduino POV
+
+int getInput();
+void moveMotors(char);
+void pick();
 
 void setup() {  
   pinMode(stepPin1, OUTPUT);
@@ -20,15 +23,18 @@ void setup() {
   BT.begin(9600);
 }
 
-
 void loop() {
-  getInput();
+  while(getInput());
   while(Serial.available() == 0);
-  moveMotors();
-  Serial.print('f');  
+  char inChar = '\0';
+  while(inChar != 'x') {
+    inChar = Serial.read();
+    moveMotors(inChar);
+  }
 }
 
 int getInput() {
+  char inChar = '\0';
   while(BT.available()) {
     inChar = (char)BT.read();
     if(inChar >= '1' && inChar <= '6') {
@@ -39,32 +45,38 @@ int getInput() {
   return 0;
 }
 
-void moveMotors() {
-  while(Serial.available()){
-    if(Serial.read()==int('l')) {
-      digitalWrite(dirPin1, HIGH);
-      digitalWrite(stepPin1, HIGH);
-      delay(stepTime);
-    }
-    if(Serial.read()==int('r')) {
-      digitalWrite(dirPin1, LOW);
-      digitalWrite(stepPin1, HIGH);
-      delay(stepTime);
-    }
-    if(Serial.read()==int('u')) {
-      digitalWrite(dirPin2, HIGH);
-      digitalWrite(stepPin2, HIGH);
-      delay(stepTime);
-    }
-    if(Serial.read()==int('d')) {
-      digitalWrite(dirPin2, LOW);
-      digitalWrite(stepPin2, HIGH);
-      delay(stepTime);
-    }
-    if(Serial.read()==int('p')){
-      pick();
+void moveMotors(char inChar) {
+  if(inChar == 'l') {
+    digitalWrite(dirPin1, HIGH);
+    digitalWrite(stepPin1, HIGH);
+    delay(stepTime);
+    digitalWrite(stepPin1, LOW);
+    delay(stepTime);
   }
- }
+  if(inChar == 'r') {
+    digitalWrite(dirPin1, LOW);
+    digitalWrite(stepPin1, HIGH);
+    delay(stepTime);
+    digitalWrite(stepPin1, LOW);
+    delay(stepTime);
+  }
+  if(inChar == 'u') {
+    digitalWrite(dirPin2, HIGH);
+    digitalWrite(stepPin2, HIGH);
+    delay(stepTime);
+    digitalWrite(stepPin2, LOW);
+    delay(stepTime);
+  }
+  if(inChar == 'd') {
+    digitalWrite(dirPin2, LOW);
+    digitalWrite(stepPin2, HIGH);
+    delay(stepTime);
+    digitalWrite(stepPin2, LOW);
+    delay(stepTime);
+  }
+  if(inChar == 'p') {
+    pick();
+  }
 }
 void pick() {
   delay(1000);
