0d50097937017e4503667f51aabcf0562330f898
anshuln <38972131+anshuln@users.noreply.github.com>
2018-07-01 14:40:43 +0530

    Update Main_Laptop.py

--- Main_Laptop.py
+++ Main_Laptop.py
@@ -1,7 +1,7 @@
 import serial
 import Sliding_Windows as SWL
 import pyautogui
-
+import imaplib
 ser = serial.Serial('COM4',9600,timeout=None)
 dictionary = {'1':'crocin', '2':'digene', '3':'flagyl', '4':'norflox', '5':'wikoryl'}
 size=[400,400]
@@ -14,7 +14,21 @@ def capture():
 	pyautogui.click(x=672,y=518)
 	pyautogui.PAUSE=2.5
 	pyautogui.click(x=1037,y=162)
-
+def read_input_from_email():
+	imapObj=imaplib.IMAP4_SSL('imap.gmail.com',993)
+	imapObj.login('ITSP35@gmail.com','ITSP35')
+	imapObj.select('INBOX')
+	unread_msg_nums=[]
+	while len(unread_msg_nums)==0:
+		status, response = imap.search(None, 'INBOX', '(UNSEEN)')
+		unread_msg_nums = response[0].split()
+		time.sleep(3)
+	for id in unread_msg_nums:
+		_, response = imap.fetch(id, '(UID BODY[TEXT])')
+		imap.store(e_id, '+FLAGS', '\Seen')
+	print(response[0][1])
+	return(response[0][1])
+	
 def get_string_from_coords(coords):
 	x = int((coords[0]/size[0])*steps)
 	y = int((coords[1]/size[1])*steps)
@@ -24,7 +38,8 @@ def get_string_from_coords(coords):
 def getName():
 	#name = dictionary[ser.read()]
 	print("Awaiting Input...")
-	inString = chr(int(ser.read(2)))
+# 	inString = chr(int(ser.read(2)))
+	inString=read_input_from_email()
 	print("Input received")
 	return dictionary[inString]
 
